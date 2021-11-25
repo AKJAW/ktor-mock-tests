2636e2ea79b82d6df885580c917635c6e8b6ccd3
AKJAW <akjaworski1@gmail.com>
2021-11-25 18:08:48 +0100

    Add basic search results

--- app/src/main/java/com/akjaw/ktor/mock/tests/ui/SearchScreen.kt
+++ app/src/main/java/com/akjaw/ktor/mock/tests/ui/SearchScreen.kt
@@ -1,19 +1,22 @@
 package com.akjaw.ktor.mock.tests.ui
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement.Absolute.spacedBy
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
@@ -30,6 +33,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.akjaw.ktor.mock.tests.ui.theme.KtorMockTestsTheme
 
 @Composable
@@ -39,11 +43,32 @@ fun SearchScreen() {
     }
     val context = LocalContext.current
 
-    fun onSubmit(keyword: String) {
-        Toast.makeText(context, "Searched for $keyword", Toast.LENGTH_SHORT).show()
+    fun onSubmit() {
+        Toast.makeText(context, "Searched for $searchKeyword", Toast.LENGTH_SHORT).show()
         searchKeyword = ""
     }
 
+    val searchResult = if (searchKeyword.isEmpty()) {
+        listOf()
+    } else {
+        previewResult.filter { it.contains(searchKeyword) }
+    }
+
+    SearchScreenContent(
+        searchResult,
+        searchKeyword,
+        { searchKeyword = it },
+        ::onSubmit
+    )
+}
+
+@Composable
+private fun SearchScreenContent(
+    searchResults: List<String>,
+    searchKeyword: String = "",
+    onSearchKeywordChange: (String) -> Unit = {},
+    onSubmit: () -> Unit = {}
+) {
     Scaffold(Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier.fillMaxWidth(),
@@ -52,16 +77,20 @@ fun SearchScreen() {
             Spacer(modifier = Modifier.height(16.dp))
             SearchInput(
                 value = searchKeyword,
-                onValue = {searchKeyword = it},
-                onSubmit = { onSubmit(searchKeyword) }
+                onValue = onSearchKeywordChange,
+                onSubmit = { onSubmit() }
             )
+            SearchResult(searchResults)
         }
     }
-
 }
 
 @Composable
-private fun SearchInput(value: String, onValue: (String) -> Unit, onSubmit: () -> Unit) {
+private fun SearchInput(
+    value: String,
+    onValue: (String) -> Unit,
+    onSubmit: () -> Unit
+) {
     Row(verticalAlignment = Alignment.CenterVertically) {
         TextField(
             value = value,
@@ -82,11 +111,40 @@ private fun SearchInput(value: String, onValue: (String) -> Unit, onSubmit: () -
     }
 }
 
+@Composable
+fun SearchResult(searchResult: List<String>) {
+    LazyColumn(
+        modifier = Modifier.padding(8.dp).fillMaxHeight(),
+        verticalArrangement = spacedBy(6.dp)
+    ) {
+        searchResult.forEach { text ->
+            item {
+                Card {
+                    Text(
+                        text = text,
+                        modifier = Modifier.fillMaxWidth().padding(4.dp),
+                        fontSize = 20.sp
+                    )
+                }
+            }
+        }
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun EmptySearchScreenPreview() {
     KtorMockTestsTheme {
-        SearchScreen()
+        SearchScreenContent(searchResults = listOf())
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun SearchScreenWithResultsPreview() {
+    KtorMockTestsTheme {
+        SearchScreenContent(searchResults = previewResult)
+    }
+}
+
+val previewResult = listOf("One", "Two", "Three", "Four")
